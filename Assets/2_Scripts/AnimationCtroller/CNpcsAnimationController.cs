8c760d4dfdb2a02cc81ba145b63002ebb0b57774
KPPAP <joeyyoo81@gmail.com>
2017-01-22 08:44:30 +0900

    애니메이션 컨트롤러 수정(애니메이션 실행시간 변경)

--- Assets/2_Scripts/AnimationCtroller/CNpcsAnimationController.cs
+++ Assets/2_Scripts/AnimationCtroller/CNpcsAnimationController.cs
@@ -2,77 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CNpcsAnimationController : MonoBehaviour {
+public class CNpcsAnimationController : MonoBehaviour
+{
 
-	Animator _ani;
-	List<string> _idles = new List<string>();
-	Task _idleMachine;
+    Animator _ani;
+    List<string> _idles = new List<string>();
+    Task _idleMachine;
 
-	/// <summary>
-	/// Awake is called when the script instance is being loaded.
-	/// </summary>
-	void Awake()
-	{
-		_ani = GetComponent<Animator>();
-	}
-	// Use this for initialization
-	void Start () {
-		_idles.Add("Idle1");
-		_idles.Add("Idle2");
-		_idles.Add("Idle3");
-		_idles.Add("Idle4");
-		StartMachine();
-	}
-	
-	void StartMachine()
-	{
-		_idleMachine = new Task(IdleMachine_Co());
-	}
-	void StopMachine()
-	{
-		if(_idleMachine != null && _idleMachine.Running)
-		{
-			_idleMachine.Stop();
-		}
-	}
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        _ani = GetComponent<Animator>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        _idles.Add("Idle1");
+        _idles.Add("Idle2");
+        _idles.Add("Idle3");
+        _idles.Add("Idle4");
+        StartMachine();
+    }
 
-	void PauseMachine()
-	{
-		if(_idleMachine != null && _idleMachine.Running)
-		{
-			_idleMachine.Pause();
-		}
-	}
-	void UnPauseMachine()
-	{
-		if(_idleMachine != null && _idleMachine.Paused)
-		{
-			_idleMachine.Unpause();
-		}
-	}
-	
-	IEnumerator IdleMachine_Co()
-	{
-		while (true)
-		{
-			int randomIdleString = Random.Range(0,_idles.Count);
-			_ani.SetTrigger(_idles[randomIdleString]);
-			float delayTime = Random.Range(1.5f,2.5f);
-			yield return new WaitForSeconds(delayTime);
-		}
-	}
-	public void TurnIdle()
-	{
-		_ani.SetBool("noAct",true);
-		_ani.SetBool("Move",false);
-		UnPauseMachine();
-	}
+    void StartMachine()
+    {
+        _idleMachine = new Task(IdleMachine_Co());
+    }
+    void StopMachine()
+    {
+        if (_idleMachine != null && _idleMachine.Running)
+        {
+            _idleMachine.Stop();
+        }
+    }
 
-	public void TurnMove()
-	{
-		PauseMachine();
-		_ani.SetBool("noAct",false);
-		_ani.SetBool("Move",true);
-	}
-	
+    void PauseMachine()
+    {
+        if (_idleMachine != null && _idleMachine.Running)
+        {
+            _idleMachine.Pause();
+        }
+    }
+    void UnPauseMachine()
+    {
+        if (_idleMachine != null && _idleMachine.Paused)
+        {
+            _idleMachine.Unpause();
+        }
+    }
+
+    IEnumerator IdleMachine_Co()
+    {
+        float delayTime = Random.Range(0f, 10f);
+        yield return new WaitForSeconds(delayTime);
+        int randomIdleString = Random.Range(0, _idles.Count);
+        _ani.SetTrigger(_idles[randomIdleString]);
+        while (true)
+        {
+            delayTime = Random.Range(10f, 15f);
+            yield return new WaitForSeconds(delayTime);
+            randomIdleString = Random.Range(0, _idles.Count);
+            _ani.SetTrigger(_idles[randomIdleString]);
+        }
+    }
+    public void TurnIdle()
+    {
+        _ani.SetBool("noAct", true);
+        _ani.SetBool("Move", false);
+        UnPauseMachine();
+    }
+
+    public void TurnMove()
+    {
+        PauseMachine();
+        _ani.SetBool("noAct", false);
+        _ani.SetBool("Move", true);
+    }
+
 }
