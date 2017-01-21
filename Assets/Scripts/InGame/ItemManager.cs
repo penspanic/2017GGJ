f0d6b925f0c57abdd779685ecfff8a7f7a03c3b0
penspanic <penspanic@gmail.com>
2017-01-22 01:24:24 +0900

    나머지 데이터 커밋

---
+++ Assets/Scripts/InGame/ItemManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pattern;
+
+public enum ItemType
+{
+    None,
+    Instigator,
+}
+
+public class ItemManager : Singleton<ItemManager>
+{
+    public ItemType currentSelect
+    {
+        get { return _currentSelect; }
+    }
+
+    ItemType _currentSelect;
+
+    public static readonly int AmplierItemCost = 100;
+
+    int currentRemainCash = 0;
+
+    public void TurnItem(ItemType item)
+    {
+        _currentSelect = item;
+    }
+
+    public bool IsSelected()
+    {
+        // 갯수 처리
+        // 떨궜을 때 NONE으로 변경
+        return _currentSelect.Equals(ItemType.None) ? false : true;
+    }
+
+    void Awake()
+    {
+        _currentSelect = ItemType.None;
+    }
+
+    public void SetCash(int cash)
+    {
+        currentRemainCash = cash;
+    }
+}
